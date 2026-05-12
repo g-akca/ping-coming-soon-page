7463c525efbbeea42fa99b438e6452f56bf916ab
g-akca <gok.akca66@gmail.com>
2026-05-12 21:11:07 +0300

    Fix styles

--- src/components/SocialMediaButton.jsx
+++ src/components/SocialMediaButton.jsx
@@ -2,8 +2,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function SocialMediaButton({ icon }) {
   return (
-    <button className="w-8 aspect-square rounded-full border-blue-200 border flex justify-center items-center cursor-pointer hover:border-blue-500 transition-all">
-      <FontAwesomeIcon icon={icon} className="text-[15px] text-blue-500" />
+    <button 
+      className="
+        w-8 aspect-square rounded-full border-blue-200 border flex justify-center items-center cursor-pointer 
+        hover:border-blue-500 hover:bg-blue-500 transition-all duration-300 group
+      "
+    >
+      <FontAwesomeIcon icon={icon} className="text-[15px] text-blue-500 group-hover:text-white transition-all duration-300" />
     </button>
   )
 }
