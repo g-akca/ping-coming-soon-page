1e70f3e0557b79b6046fecf551acebfa2fb0065b
g-akca <gok.akca66@gmail.com>
2026-05-12 15:47:24 +0300

    Give MainSection structure and main styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="text-base leading-base min-h-screen pt-21 flex flex-col justify-between gap-6">
+    <div className="text-base leading-base text-blue-950 min-h-screen pt-21 pb-8 px-7 flex flex-col justify-between gap-6">
       <MainSection />
 
       <Footer />
--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,7 +1,29 @@
+import dashboardImg from "/images/illustration-dashboard.png";
+import logoImg from "/images/logo.svg";
+
 function MainSection() {
   return (
-    <>
-    </>
+    <main className="flex flex-col gap-18">
+      <div className="px-2 flex flex-col gap-10">
+        <div className="flex flex-col gap-10 items-center">
+          <img src={logoImg} alt="Logo image" className="h-4" />
+
+          <div className="flex flex-col gap-2 text-center">
+            <h2>We are launching <strong>soon!</strong></h2>
+
+            <p>Subscribe and get notified</p>
+          </div>
+        </div>
+
+        <form className="px-2.5 flex flex-col gap-2">
+          <input type="text" placeholder="Your email address..." />
+
+          <button type="submit" className="text-center">Notify Me</button>
+        </form>
+      </div>
+
+      <img src={dashboardImg} alt="Dashboard illustration" />
+    </main>
   )
 }
 
