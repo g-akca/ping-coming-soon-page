fcad2319cd6efff98925b946c9878e3b1841470c
g-akca <gok.akca66@gmail.com>
2026-08-13 21:16:32 +0300

    Add platform prop to SocialMediaButton for accessibility

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,9 +5,9 @@ function Footer() {
   return (
     <footer className="flex flex-col gap-6 items-center">
       <div className="flex gap-4">
-        <SocialMediaButton icon={faFacebookF} />
-        <SocialMediaButton icon={faTwitter} />
-        <SocialMediaButton icon={faInstagram} />
+        <SocialMediaButton icon={faFacebookF} platform="Facebook" />
+        <SocialMediaButton icon={faTwitter} platform="Twitter" />
+        <SocialMediaButton icon={faInstagram} platform="Instagram" />
       </div>
 
       <p className="text-[12px] leading-base text-gray-400 font-light">© Copyright Ping. All rights reserved.</p>
--- src/components/SocialMediaButton.jsx
+++ src/components/SocialMediaButton.jsx
@@ -1,8 +1,9 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function SocialMediaButton({ icon }) {
+function SocialMediaButton({ icon, platform }) {
   return (
-    <button 
+    <button
+      aria-label={platform}
       className="
         w-8 aspect-square rounded-full border-blue-200 border flex justify-center items-center cursor-pointer 
         hover:border-blue-500 hover:bg-blue-500 transition-all duration-300 group
